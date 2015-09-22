dce3be6fd6151db4c9f7604e0cdd6d5eafe1a349
beck318love <supersenior318@gmail.com>
2015-09-22 11:04:48 -0700

    finish no. 1

---
+++ db/migrate/20150922084103_add_first_lastname_to_users.rb
@@ -0,0 +1,7 @@
+class AddFirstLastnameToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :phone_num, :string
+  end
+end
